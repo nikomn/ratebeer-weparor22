edc3149d8f8d2f3f1b9acfe0037b17c4a127c5f3
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2022-12-07 19:19:46 +0200

    Viikko 3 Tehtävä 15

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true,
             length: { minimum: 3, maximum: 30 }
   validates :password, length: { minimum: 4 }, format: { with: /.*([A-Z]+.*[0-9]|[0-9]+.*[A-Z])/, on: :create }
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
   has_secure_password
 end
