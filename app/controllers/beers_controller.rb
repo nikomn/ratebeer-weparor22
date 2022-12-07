c8eb6a3a310b33a6da981c8817b91dfea48f9c97
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2022-12-07 18:09:13 +0200

    Viikko 3 Tehtävä 8

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -29,10 +29,12 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to beers_path, notice: "Beer was successfully created." }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
         format.json { render :show, status: :created, location: @beer }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        @breweries = Brewery.all
+        @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Lowalcohol"]
+        format.html { render :new }
         format.json { render json: @beer.errors, status: :unprocessable_entity }
       end
     end
