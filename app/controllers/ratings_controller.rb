fff47bcf60bff42b5cb182d6eedfaf0cae21bcf4
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2022-12-07 17:44:57 +0200

    Viikko 3 Tehtävä 6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,11 +10,15 @@ class RatingsController < ApplicationController
 
   def create
     # binding.pry
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+    # rating = Rating.create params.require(:rating).permit(:score, :beer_id)
     # Rating.create params.require(:rating).permit(:score, :beer_id)
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-    redirect_to ratings_path
+    # session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
+    # redirect_to ratings_path
     # redirect_to "http://www.cs.helsinki.fi"
+    rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+    rating.user = current_user
+    rating.save
+    redirect_to current_user
   end
 
   def destroy
